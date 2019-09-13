96764b46c4bceb07fd452838a148ab8472814669
Alexander Belashev <alexbelashew@gmail.com>
2019-09-13 16:19:26 +0300

    Вынесена переменная  в приватную переменную класса, заменены вызовы переменной  на обращение к приватной переменной

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -10,26 +10,32 @@ use App\System\Base\Controller;
  * Контроллер пользователей
  * @todo: вынести переменную $list в приватную переменную класса
  * @todo: заменить вызов переменной $list на обращение к приватной переменной $list
- * @todo: реализовать приватный метод find возвращающий по запрошенному ip элемент списка $list
+ * @todo: реализовать приватный метод find возвращающий по запрошенному id элемент списка $list
  * @todo: в actionView задействовать метод find для поиска по запрошенному id
  * @todo: в представлении users/view вывести все данные запрошенного пользователя
  */
 class UsersController extends Controller
 {
+    private $list;
+
+    private function find($id)
+    {
+        
+    } 
+
     public function actionIndex()
     {
-        $list = [
-            new User(70, 'Alexey',  'alexey@gmail.com'),
-            new User(71, 'Alexey1', 'alexey1@gmail.com'),
-            new User(72, 'Alexey2', 'alexey2@gmail.com'),
-            new User(73, 'Alexey3', 'alexey3@gmail.com'),
-            new User(74, 'Alexey4', 'alexey4@gmail.com'),
-            new User(75, 'Alexey5', 'alexey5@gmail.com'),
-            new User(76, 'Alexey6', 'alexey6@gmail.com'),
-        ];
-
+        $this->list = [
+            new User(1, 'Alexey',  'alexey@gmail.com'),
+            new User(2, 'John', 'John@gmail.com'),
+            new User(3, 'Phil', 'Phil@gmail.com'),
+            new User(4, 'Sonya', 'Sonya@gmail.com'),
+            new User(5, 'Vasiliy', 'Vasiliy@gmail.com'),
+            new User(6, 'Judy', 'Judy@gmail.com'),
+            new User(7, 'Den', 'Den@gmail.com'),
+        ];       
         return $this->render('users/index', [
-            'list' => $list,
+            'list' => $this->list,
         ]);
     }
 
